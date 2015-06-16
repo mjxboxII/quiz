b917d202184c072aaf4c3b77d577ff0243a9155d
MJose <mjxboxII@gmail.com>
2015-06-16 16:34:27 +0200

    DB (sequelize.js y SQLite) y despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,27 @@
+//Importar modelo para acceder a la BBDD
+var models = require('../models/models.js');
+
 //GET /quizes/question
 exports.question=function(req,res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().success(function(quiz){
+			res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	})	
 };
 
 //GET /quizes/answer
 exports.answer=function(req,res){
-	
-	var patt = /^Roma$/i;
+
 	var _miresp = req.query.respuesta.replace(/[ ]+/g," ");
 		_miresp = _miresp.replace(/^[ ]+/g,'');
 		_miresp = _miresp.replace(/[ ]+$/g,'');
 
 	_miresp = _miresp.charAt(0).toUpperCase()+_miresp.slice(1);
 	
-	if (patt.test(_miresp)){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+	models.Quiz.findAll().success(function(quiz){
+		if (_miresp===quiz[0].respuesta){
+			res.render('quizes/answer', {respuesta: 'Correcto'});
+		} else {
+			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		}
+	})
 };
