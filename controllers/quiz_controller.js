75629edf6574137a5ee8778c1ef5a5e4396fb30d
MJose <mjxboxII@gmail.com>
2015-06-17 14:00:47 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -117,5 +117,9 @@ exports.update = function(req, res){
 		req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
 		res.redirect('/quizes')}) //Redireccion HTTP (URL relativo) lista de preguntas
 	}
+};
 
+//DELETE /quizes/:id
+exports.destroy = function(req, res){
+	req.quiz.destroy().then(function(){ res.redirect('/quizes');}).catch(function(error){ next(error) });
 };
